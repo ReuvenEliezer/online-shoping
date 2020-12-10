0d6338773bdb3b077b434de0ea95b26b6d6c55aa
Eliezer <reuvene@mprest.com>
2020-12-10 13:44:57 +0200

    add exception handling

--- account-service/src/main/java/com/controllers/ControllerExceptionHandler.java
+++ account-service/src/main/java/com/controllers/ControllerExceptionHandler.java
@@ -3,34 +3,39 @@ package com.controllers;
 import com.exception.ErrorMessage;
 import com.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 
 import java.util.Date;
 
-@ControllerAdvice
+//@ResponseBody
+//@ControllerAdvice
+@RestControllerAdvice
+/**
+ *https://bezkoder.com/spring-boot-restcontrolleradvice/
+ //https://bezkoder.com/spring-boot-controlleradvice-exceptionhandler/
+ */
 public class ControllerExceptionHandler {
+
+    @ResponseStatus(value = HttpStatus.NOT_FOUND)
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorMessage> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(
+    public ErrorMessage resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
+        return new ErrorMessage(
                 HttpStatus.NOT_FOUND.value(),
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
-
-        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
-        ErrorMessage message = new ErrorMessage(
+    @ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorMessage globalExceptionHandler(Exception ex, WebRequest request) {
+        return new ErrorMessage(
                 HttpStatus.INTERNAL_SERVER_ERROR.value(),
                 new Date(),
                 ex.getMessage(),
                 request.getDescription(false));
-
-        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
